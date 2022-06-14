528c512aa50cf7d5e28720d291f9fbf47df55b7f
benassiPb <benassipb@gmail.com>
2022-06-14 16:46:22 -0700

    cart api routes

---
+++ server/api/cart_items.js
@@ -0,0 +1,64 @@
+const router = require('express').Router()
+const { models: { User, Order, Plant, Cart_Item } } = require('../db')
+
+
+// display cart (all cart_items belonging to :userId)
+router.get('/:userId', async (req, res, next) => {
+  let cart
+  try {
+    cart = await Cart_Item.findAll({
+      where: {
+        userId: req.params.userId
+      },
+      include: [User]
+    });
+  }
+  catch (err) {
+    next(err);
+  }
+  res.json(cart);
+})
+
+//create new cart_item
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).send(await Cart_Item.create(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//delete cart_item from cart
+router.delete('/:cart_itemId', async (req, res, next) => {
+
+  try {
+    const cart_item = await Cart_Item.findByPk(req.params.cart_itemId)
+    cart_item.destroy()
+    res.send(cart_item)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// increment quantity of item in cart
+router.put('/inc/:cart_itemId', async (req, res, next) => {
+  try {
+    const cart_item = await Cart_Item.findByPk(req.params.cart_itemId)
+    res.send(await cart_item.update({ quantity: quantity + 1 }))
+  } catch (error) {
+    next(error)
+  }
+})
+
+// decrement quantity of item in cart
+router.put('/dec/:cart_itemId', async (req, res, next) => {
+  try {
+    const cart_item = await Cart_Item.findByPk(req.params.cart_itemId)
+    res.send(await cart_item.update({ quantity: quantity - 1 }))
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+module.exports = router
